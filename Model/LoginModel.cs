06e7f2f5d1f6336e31db9056552de2d10f74334e
HiwarkarHira98 <hirahiwarkar@gmail.com>
2025-03-05 10:39:12 +0530

    Initial commit for new repository

---
+++ Model/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyApiJwt.Model
+{
+    public class LoginModel
+    {
+      
+            public required string Username { get; set; }
+            public required string Password { get; set; }
+        
+    }
+}
